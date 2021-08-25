12621a414bbd02f4cc4793d6a0498db3559105fc
pradhanrahul <57364091+pradhanrahul@users.noreply.github.com>
2021-08-25 16:42:24 +0530

    24/8/21 exam

---
+++ test.js
@@ -0,0 +1,53 @@
+function Data1()
+{
+    document.getElementById("text").style.color="red";
+}
+document.getElementById("btn1").addEventListener("click",Data1)
+
+function Data2()
+{
+    document.getElementById("text").style.color="green";
+}
+document.getElementById("btn2").addEventListener("click",Data2)
+
+function Data3()
+{
+    document.getElementById("text").style.color="blue";
+}
+document.getElementById("btn3").addEventListener("click",Data3)
+
+function text1()
+{
+    document.getElementById("text").style.fontSize="20px";
+}
+document.getElementById("btna").addEventListener("click",text1)
+
+function text2()
+{
+    document.getElementById("text").style.fontSize="30px";
+}
+document.getElementById("btnb").addEventListener("click",text2)
+
+function text3()
+{
+    document.getElementById("text").style.fontSize="40px";
+}
+document.getElementById("btnc").addEventListener("click",text3)
+
+function text1()
+{
+    document.getElementById("text").style.fontFamily="Arial";
+}
+document.getElementById("btnd").addEventListener("click",text1)
+
+function text2()
+{
+    document.getElementById("text").style.fontFamily="calibri";
+}
+document.getElementById("btne").addEventListener("click",text2)
+
+function text3()
+{
+    document.getElementById("text").style.fontFamily="Fixedsys";
+}
+document.getElementById("btnf").addEventListener("click",text3)
